cdd5d21c60fd7440f12752548d052b7091c5452c
Le Thi Le Kieu <ltlkieu1207@gmail.com>
2021-01-07 10:58:28 +0700

    them file khoi tao

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int tongBT(int a, int b);
+int hieuBT(int a, int b);
+int tichBT(int a, int b);
+float thuongBT(int a, int b);
+
+
+int main()
+{
+	int a, b ;
+
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	cout << "\nNhap vao a:";
+	cin>> a;
+	cout << "\nNhap vao b:";
+	cin>> b;
+
+	system("pause");
+	return 0;
+}
